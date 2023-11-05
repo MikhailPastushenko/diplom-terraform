3e9f9607cc95c19744d2218d2e4502c70fd27211
mikhail pastushenko <mikhail.pastushenko@gmail.com>
2023-11-05 14:01:20 +0300

    output

--- main.tf
+++ main.tf
@@ -13,29 +13,31 @@ resource "yandex_vpc_subnet" "public-subnet" {
   name           = "public-subnet-${local.env}"
   zone           = var.YC_ZONE
   network_id     = yandex_vpc_network.vpc-network.id
-  v4_cidr_blocks = (local.env == "prod" ? ["192.168.10.0/24"] : ["192.168.30.0/24"] )
+  v4_cidr_blocks = (local.env == "prod" ? ["192.168.10.0/24"] : ["192.168.20.0/24"] )
 }
 
 # private subnet
-resource "yandex_vpc_subnet" "private-subnet" {
-  name           = "private-subnet-${local.env}"
-  zone           = var.YC_ZONE
-  network_id     = yandex_vpc_network.vpc-network.id
-  v4_cidr_blocks = (local.env == "prod" ? ["192.168.20.0/24"] : ["192.168.40.0/24"] )
-}
+#resource "yandex_vpc_subnet" "private-subnet" {
+#  name           = "private-subnet-${local.env}"
+#  zone           = var.YC_ZONE
+#  network_id     = yandex_vpc_network.vpc-network.id
+#  v4_cidr_blocks = (local.env == "prod" ? ["192.168.30.0/24"] : ["192.168.40.0/24"] )
+#}
 
 ## kubernetes cluster VMs
 
 
 # master node
 resource "yandex_compute_instance" "master" {
-  name = "master-${local.env}"
-  hostname = "master-${local.env}"
+  count = 1
+  name = "master-${count.index}-${local.env}"
+  hostname = "master-${count.index}-${local.env}"
+
   zone     = var.YC_ZONE
   platform_id = "standard-v3"
   resources {
-    cores  = 2
-    memory = 2
+    cores  = 4
+    memory = 8
     core_fraction = 100
   }
   boot_disk {
@@ -54,38 +56,11 @@ resource "yandex_compute_instance" "master" {
   }
 }
 
-
 # worker node
-resource "yandex_compute_instance" "worker1" {
-  name = "worker1-${local.env}"
-  hostname = "worker1-${local.env}"
-  zone     = var.YC_ZONE
-  platform_id = "standard-v3"
-  resources {
-    cores  = 2
-    memory = 2
-    core_fraction = 100
-  }
-  boot_disk {
-    initialize_params { 
-      image_id = "fd80bm0rh4rkepi5ksdi"
-      size = 50
-    }
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.public-subnet.id
-    nat = true
-  }
-  metadata = {
-    user-data = "${file("meta.yaml")}"
-    serial-port-enable = 1
-  }
-}
-
-# worker node
-resource "yandex_compute_instance" "worker2" {
-  name = "worker2-${local.env}"
-  hostname = "worker2-${local.env}"
+resource "yandex_compute_instance" "worker" {
+  count = 2
+  name = "worker-${count.index}-${local.env}"
+  hostname = "worker-${count.index}-${local.env}"
   zone     = var.YC_ZONE
   platform_id = "standard-v3"
   resources {
@@ -108,4 +83,16 @@ resource "yandex_compute_instance" "worker2" {
     serial-port-enable = 1
   }
 }
+
+
+output "master_ip_list" {
+  value = yandex_compute_instance.master[*].network_interface.0.ip_address
+
+}
+
+output "worker_ip_list" {
+  value = yandex_compute_instance.worker[*].network_interface.0.ip_address
+
+}
+
 
--- variables.tf
+++ variables.tf
@@ -3,6 +3,15 @@ variable YC_TOKEN {}
 variable YC_CLOUD_ID {}
 variable YC_FOLDER_ID {}
 variable YC_ZONE {}
+variable worker_count {
+  type = number   
+  default = 10
+}
+variable master_count {
+  type = number
+  default = 3
+}
+
 #  variable env {
 #  type = string
 #  default = terraform.workspace
